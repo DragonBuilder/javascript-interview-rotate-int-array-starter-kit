b90d33bea5f737cace8724049d9c0cd5555d6eda
Aneesh Anil <aneesh@geektrust.in>
2024-06-17 18:17:45 +0530

    giving candidate a very specific `rotateArray` method to fill in.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 
+/**
+ * 
+ * @param {int[]} arr - a sequence of integers
+ * @returns {int[]} - the rotated array
+ * 
+ * This function will be called by our system. 
+ * So fill in your code inside this function.
+ * 
+ */
+function rotateArray(arr) {
+    // WRITE YOUR CODE HERE....
+
+}
+
+
+
+
 /*
-* This is the main entry point for the program. It will parse the input for you.
-* You don't need to change this.
+* **************************************************************************************
+*                          DO NOT EDIT THE CODE BELOW.
+* ***************************************************************************************
+* This code is used to boostrap your solution to be checked for correctness by our system.
 */
 function main() {
-    const input = process.argv.slice(2)
-    if (input.length === 0) {
-        throw new Error("No argument passed")
-    }
+    const input = process.argv.slice(1)
+    
     const arr = JSON.parse(input[0])
-    handle(arr)
+
+    const got = rotateArray(arr)
+    console.log(JSON.stringify(got))
 }
 
-
-/*
- * Use this method to write your solution. 
- * arr - Is an array of Integers
- */
-function handle(arr) {
-    console.log(arr)
-    //TODO: implement the logic to handle each input
-}
 
 main()
